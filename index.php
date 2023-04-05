4b08d1f35153157cca91fb0a5f8334283771cfd8
Rogiero <Cruzjr.rogelio@gmail.com>
2023-04-05 10:44:32 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>VMC CSAT</title>
+	<link rel="stylesheet" type="text/css" href="./index.css">
+	<link rel="shortcut icon" href="./src/vmclogo.png">		
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">		
+	<meta charset="utf-16">
+</head>
+<body>
+	<!--Page Header-->
+	<div class="pageHeaderWrap">
+		<div class="pageHeaderTxt">Data Provicy Policy</div>
+		<div class="pageHeaderSubtxt">To learn more about data privacy visit Republic Act 10173 – Data Privacy Act of 2012</div>
+	</div>
+	<!--Page Header-->
+
+
+	<!--Page Content-->
+	<div class="pageContentWrapFlex">
+		<!--_Policy-->
+		<div class="policyDetailsItem">
+			<!--_ _Policy date details-->
+			<div class="policyDateDetailsWrap">				
+				<div class="policyDateTxt">Effective March 22, 2023</div>
+				<div class="policyUpToDateTxt">Our Data Privacy Policy is up to date.</div>
+			</div>
+			<!--_ _Policy date details-->
+
+
+			<!--_ _Policy big label-->
+			<div class="policyBigLabelTxt">Your Privacy Matters</div>
+			<!--_ _Policy big label-->
+
+
+			<!--_ _Policy Paragraph-->
+			<div class="policyParagraph-Class">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+				<br><br>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+			</div>
+			<!--_ _Policy Paragraph-->
+
+
+			<!--_ _Accept terms and agreement-->
+			<div class="policyAgreeTermsWrap">
+				<div class="cusCheckBoxPaper_RoClass agreeTermsCheckbox-Class">
+					<label for="agreeTermsCheckbox-Id">I agree to the Terms of Use and Data Privacy Policy:<div class="boxme_RoClass"><img src="./src/checkIcon.png"></div></label>
+					<input type="checkbox" id="agreeTermsCheckbox-Id" onchange="checkCusCheckBox(this)" autocomplete="off">
+				</div>
+
+				<button class="normButton_RoClass" style="margin: 5px auto;">Rate our service</button>
+			</div>
+			<!--_ _Accept terms and agreement-->			
+		</div>		
+		<!--_Policy-->		
+	</div>
+	<!--Page Content-->
+
+	<script type="text/javascript" src="./Rogrid/Scripts/RogridNodeScript_Index.js"></script>	
+	<script type="text/javascript" src="./index.js"></script>	
+</body>
+</html>
