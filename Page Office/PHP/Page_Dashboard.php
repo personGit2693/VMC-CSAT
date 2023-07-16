b782ce267750548ab14b98d9769d2405eae005a2
Rogiero <Cruzjr.rogelio@gmail.com>
2023-07-17 07:17:58 +0800

    Included updates from Rogrid new Side Nav.

---
+++ Page Office/PHP/Page_Dashboard.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CSAT Dashboard</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">	
+	<link rel="stylesheet" type="text/css" href="../CSS/Page_Dashboard.css">
+	<link rel="shortcut icon" href="../../src/vmclogo.png">
+</head>
+<body>
+
+</body>
+</html>
