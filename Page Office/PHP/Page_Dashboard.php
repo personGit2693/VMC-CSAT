b8824dfcc8ca9614fa1f81aa3d8e05c0ad5955dd
Rogiero <Cruzjr.rogelio@gmail.com>
2023-07-20 16:44:28 +0800

    Dashboard included and updated Rogrid Library

--- Page Office/PHP/Page_Dashboard.php
+++ Page Office/PHP/Page_Dashboard.php
@@ -8,6 +8,68 @@
 	<link rel="shortcut icon" href="../../src/vmclogo.png">
 </head>
 <body>
+	<div class="rolayout_RoClass">
+		<div class="sidemecha_RoClass" style="--naviBgColor:#ffffff;"> 
+			<div class="sidemechaCon_RoClass">
+				<a href="" class="mainNavMenu_RoClass">					
+					<div class="mainNavMenuIconWrap_RoClass" style="--mainNavMenuIcon:url('../../src/abouticonwhite.png'); --activeMainNavMenuIcon:url('../../src/abouticongreen.png')"></div>
+					<span class="generalNavMenuText_RoClass">navigation 1</span>
+				</a>
 
+
+				<a href="" class="activeMainNavMenu_RoClass">					
+					<div class="activeMainNavMenuIconWrap_RoClass" style="--activeMainNavMenuIcon:url('../../src/abouticongreen.png')"></div>
+					<span class="generalNavMenuText_RoClass">navigation 2</span>
+				</a>
+				
+
+				<div class="mainNavMenu_RoClass" onclick="collapseMenu(this)">					
+					<div class="mainNavMenuIconWrap_RoClass" style="--mainNavMenuIcon:url('../../src/bookcontacticonwhite.png'); --activeMainNavMenuIcon:url('../../src/bookcontacticongreen.png')"></div>
+					<span class="generalNavMenuText_RoClass">navigation 3</span><div class="chevronIconWrap_RoClass"><img src="../../src/Chevron Right.png" /></div>
+				</div>
+				<div style="max-height: 0px;" class="subNavMenuWrap_RoClass">
+					<a href="" class="subNavMenu_RoClass">					
+						<div class="subNavMenuIconWrap_RoClass" style="--subNavMenuIcon:url('../../src/abouticonwhite.png'); --activeSubNavMenuIcon:url('../../src/abouticongreen.png')"></div>
+						<span class="generalNavMenuText_RoClass">navigation 3.1</span>
+					</a>
+				</div>
+	
+
+				<div class="activeMainNavMenu_RoClass" onclick="collapseMenu(this)">					
+					<div class="activeMainNavMenuIconWrap_RoClass" style="--activeMainNavMenuIcon:url('../../src/bookcontacticongreen.png')"></div>
+					<span class="generalNavMenuText_RoClass">navigation 4</span><div class="activeChevronIconWrap_RoClass"><img src="../../src/Chevron Right.png" /></div>
+				</div>
+				<div style="max-height: 1000px;" class="subNavMenuWrap_RoClass">
+					<a href="" class="subNavMenu_RoClass">					
+						<div class="subNavMenuIconWrap_RoClass" style="--subNavMenuIcon:url('../../src/abouticonwhite.png'); --activeSubNavMenuIcon:url('../../src/abouticongreen.png')"></div>
+						<span class="generalNavMenuText_RoClass">navigation 4.1</span>
+					</a>
+					<a href="" class="activeSubNavMenu_RoClass">					
+						<div class="activeSubNavMenuIconWrap_RoClass" style="--activeSubNavMenuIcon:url('../../src/abouticongreen.png')"></div>
+						<span class="generalNavMenuText_RoClass">navigation 4.2</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="mainmecha_RoClass" style="--mainBodyBgColor:#E8EFFF;">
+			<!--Page title-->
+			<div class="mainmechaCon_RoClass layoutOneHead_RoClass" style="--projectTitle_BgColor:#ffffff;">
+				<div class="burger_RoClass"><img class="burgerIcon_RoClass"></div>
+				<div class="projTitleAndUserLogin_RoClass">
+					<div class="projTitleTxt_RoClass">Dashboard</div>
+					<div class="userLogin_RoClass">
+						<div class="userLoginIconWrap_RoClass"><img src="../../src/office-building.png"></div>
+						<div class="userLoginDetails_RoClass">Office Account Details</div>
+					</div>
+				</div>
+			</div>
+			<!--Page title-->
+
+			
+		</div> 
+	</div>
+
+	<script type="text/javascript" src="../../Rogrid/Scripts/RogridNodeLayOneNavScript.js"></script>
+	<script type="text/javascript" src="../../Rogrid/Scripts/RogridNodeScript.js"></script>
 </body>
 </html>
