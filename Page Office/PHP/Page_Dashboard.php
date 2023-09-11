fa7f59cdfaceecd3c63e48132e8f6926848a7896
Rogiero <Cruzjr.rogelio@gmail.com>
2023-09-11 13:24:03 +0800

    Centered Charts on Page_Dashboard.php

--- Page Office/PHP/Page_Dashboard.php
+++ Page Office/PHP/Page_Dashboard.php
@@ -68,7 +68,7 @@ if(isset($_SESSION["accountNumber"]) && isset($_SESSION["officeId"]) && isset($_
 		<title>CSAT Dashboard</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">	
-		<link rel="stylesheet" type="text/css" href="../CSS/Page_Dashboard.css">
+		<link rel="stylesheet" type="text/css" href="../CSS/Page_Dashboard.css?v1">
 		<link rel="shortcut icon" href="../../src/vmclogo.png">
 	</head>
 	<body>
